c6de5f9a6d1afa0c947370a08379b7ff17d2b9b8
LabUser <lab@example.com>
2025-06-25 16:59:54 +0800

    Lab8 finish

--- kernel/bio.c
+++ kernel/bio.c
@@ -23,72 +23,149 @@
 #include "fs.h"
 #include "buf.h"
 
+#define BUCKETSIZE 13
+extern uint ticks;                // 全局时间
+
+/*
+一个哈希桶会对应多个buf
+用时间戳代替LRU算法，可以防止操作整个bcache用一把锁，但是需要遍历整个桶来获取最小时间戳
+对一个桶而言，如果申请时没有空闲，就在全局驱逐
+*/
+static int getidx(int blockno)
+{
+  return blockno%BUCKETSIZE;
+}
+
+// 1.修改bcache结构以适配要求，还有更改buf结构体
 struct {
-  struct spinlock lock;
+  // 哈希桶
+  struct spinlock lock[BUCKETSIZE];
+  struct spinlock evictlock;            // 全局锁，便于调试
   struct buf buf[NBUF];
 
   // Linked list of all buffers, through prev/next.
   // Sorted by how recently the buffer was used.
   // head.next is most recent, head.prev is least.
-  struct buf head;
+  // struct buf head;
+  struct buf head[BUCKETSIZE];          // 桶的头节点
 } bcache;
 
+// 2.更改bcache的binit，初始化所有桶锁
 void
 binit(void)
 {
   struct buf *b;
 
-  initlock(&bcache.lock, "bcache");
-
+  for(int i=0;i<BUCKETSIZE;i++)
+  {
+    initlock(&bcache.lock[i], "bcache");            // 初始化桶锁
+    bcache.head[i].next = &bcache.head[i];
+  }
+  initlock(&bcache.evictlock,"evictlock");          // 初始化全局锁
   // Create linked list of buffers
-  bcache.head.prev = &bcache.head;
-  bcache.head.next = &bcache.head;
+  // bcache.head.prev = &bcache.head;
+  // bcache.head.next = &bcache.head;
   for(b = bcache.buf; b < bcache.buf+NBUF; b++){
-    b->next = bcache.head.next;
-    b->prev = &bcache.head;
-    initsleeplock(&b->lock, "buffer");
-    bcache.head.next->prev = b;
-    bcache.head.next = b;
+    // b->next = bcache.head.next;
+    // b->prev = &bcache.head;
+    initsleeplock(&b->lock, "buffer");            // 初始化buffer的睡眠锁
+    // bcache.head.next->prev = b;
+    // bcache.head.next = b;
+    b->next = bcache.head[0].next;                // 初始化链表，全在head[0]上
+    if(bcache.head[0].next)
+    bcache.head[0].next = b;
   }
 }
 
 // Look through buffer cache for block on device dev.
 // If not found, allocate a buffer.
 // In either case, return locked buffer.
+// 3.修改以寻找桶，驱逐桶，抢夺桶
 static struct buf*
 bget(uint dev, uint blockno)
 {
   struct buf *b;
 
-  acquire(&bcache.lock);
+  int idx = getidx(blockno);
+  struct spinlock *lock = &bcache.lock[idx];              // 获取设备编号对应的桶锁
+  acquire(lock);
 
   // Is the block already cached?
-  for(b = bcache.head.next; b != &bcache.head; b = b->next){
+  for(b = bcache.head[idx].next; b != &bcache.head[idx]; b = b->next){
     if(b->dev == dev && b->blockno == blockno){
       b->refcnt++;
-      release(&bcache.lock);
+      b->las = ticks;
+      release(lock);
       acquiresleep(&b->lock);
       return b;
     }
   }
 
-  // Not cached.
-  // Recycle the least recently used (LRU) unused buffer.
-  for(b = bcache.head.prev; b != &bcache.head; b = b->prev){
-    if(b->refcnt == 0) {
-      b->dev = dev;
-      b->blockno = blockno;
-      b->valid = 0;
-      b->refcnt = 1;
-      release(&bcache.lock);
-      acquiresleep(&b->lock);
-      return b;
+// 未命中缓存时的驱逐,遍历全局获得las最大的buf
+  release(lock);
+  int min_las = 0x7fffffff;
+  struct buf *pre = 0;
+  int bucketidx = -1;
+  for(int i=0;i<BUCKETSIZE;i++)
+  {
+    acquire(&bcache.lock[i]);
+    int find_better = 0;
+    for(b = &bcache.head[i];b -> next != &bcache.head[i];b = b->next)
+    {
+      if(b->next->refcnt==0 && min_las > b->next->las)
+      {
+        min_las=b->next->las;
+        pre = b;
+        find_better=1;
+      }
     }
+
+    if(find_better)                   // 找到更适合的驱逐块,释放之前块的锁
+    {
+      if(bucketidx!=-1)
+      release(&bcache.lock[bucketidx]);
+      bucketidx = i;                  // 改成当前
+    }
+    else release(&bcache.lock[i]);
   }
-  panic("bget: no buffers");
+// 寻找到可以驱逐的buf，先把buf从原来的块释放，并插入当前快，然后二次检查，因为有可能其他CPU或者进程已经缓存了
+struct buf *fbuf = 0;
+if(pre!=0)
+{
+  fbuf = pre->next;
+  pre->next=pre->next->next;
+  release(&bcache.lock[bucketidx]);
+}
+if(fbuf)
+{
+  acquire(lock);
+  fbuf->next = bcache.head[idx].next;
+  bcache.head[idx].next=fbuf;
 }
 
-// Return a locked buf with the contents of the indicated block.
+// 二次检查，是否已缓存
+  for(b = bcache.head[idx].next; b != &bcache.head[idx]; b = b->next){
+    if(b->dev == dev && b->blockno == blockno){
+      b->refcnt++;
+      b->las = ticks;
+      release(lock);
+      acquiresleep(&b->lock);
+      return b;
+    }
+  }
+if(!fbuf) panic("bget: no buffers");
+// 到这里就是没缓存，直接返回fbuf
+  fbuf->dev = dev;
+  fbuf->blockno = blockno;
+  fbuf->valid = 0;
+  fbuf->refcnt = 1;
+  fbuf->las = ticks;
+  release(lock);
+  acquiresleep(&fbuf->lock);
+  return fbuf;
+}
+
+// Return a locked buf with the contents of the indicated block. 还未缓存磁盘的buf，先读磁盘
 struct buf*
 bread(uint dev, uint blockno)
 {
@@ -102,7 +179,7 @@ bread(uint dev, uint blockno)
   return b;
 }
 
-// Write b's contents to disk.  Must be locked.
+// Write b's contents to disk.  Must be locked.写磁盘
 void
 bwrite(struct buf *b)
 {
@@ -113,41 +190,38 @@ bwrite(struct buf *b)
 
 // Release a locked buffer.
 // Move to the head of the most-recently-used list.
+// 4.修改relse，只修改时间戳即可
 void
 brelse(struct buf *b)
 {
   if(!holdingsleep(&b->lock))
     panic("brelse");
 
-  releasesleep(&b->lock);
-
-  acquire(&bcache.lock);
+  int idx = getidx(b->blockno);
+  struct spinlock *lock = &bcache.lock[idx];
+  acquire(lock);
   b->refcnt--;
-  if (b->refcnt == 0) {
-    // no one is waiting for it.
-    b->next->prev = b->prev;
-    b->prev->next = b->next;
-    b->next = bcache.head.next;
-    b->prev = &bcache.head;
-    bcache.head.next->prev = b;
-    bcache.head.next = b;
-  }
-  
-  release(&bcache.lock);
+  release(lock);
+  releasesleep(&b->lock);
 }
 
 void
 bpin(struct buf *b) {
-  acquire(&bcache.lock);
+  int idx = getidx(b->blockno);
+  struct spinlock *lock = &bcache.lock[idx];
+  acquire(lock);
   b->refcnt++;
-  release(&bcache.lock);
+  b->las = ticks;
+  release(lock);
 }
 
 void
 bunpin(struct buf *b) {
-  acquire(&bcache.lock);
+  int idx = getidx(b->blockno);
+  struct spinlock *lock = &bcache.lock[idx];
+  acquire(lock);
   b->refcnt--;
-  release(&bcache.lock);
+  release(lock);
 }
 
 
--- kernel/buf.h
+++ kernel/buf.h
@@ -1,12 +1,12 @@
 struct buf {
-  int valid;   // has data been read from disk?
-  int disk;    // does disk "own" buf?
-  uint dev;
-  uint blockno;
-  struct sleeplock lock;
-  uint refcnt;
-  struct buf *prev; // LRU cache list
-  struct buf *next;
-  uchar data[BSIZE];
+  int valid;   // 缓冲数据有效性标志：1=数据已从磁盘读取且有效，0=需读取磁盘
+  int disk;    // 磁盘所有权标志：1=磁盘I/O进行中(缓冲正被读写)，0=缓冲可复用
+  uint dev;    // 设备号，标识该缓冲区对应的存储设备
+  uint blockno; // 块号，标识设备上的具体物理块位置
+  struct sleeplock lock; // 睡眠锁：保障缓冲数据访问的互斥性（允许进程睡眠等待）
+  uint refcnt;  // 引用计数：统计当前使用该缓冲区的进程/操作数量
+  // struct buf *prev; // LRU链表：指向链表中前一个缓冲区（最近最少使用算法）
+  struct buf *next; // LRU链表：指向链表中下一个缓冲区
+  int las;             // 最后一次使用的时间戳
+  uchar data[BSIZE]; // 实际数据存储区（大小=磁盘块尺寸BSIZE）
 };
-
